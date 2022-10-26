4d7f02bfbd98fb11f17a22e036a56d6affcbc237
md-sazzadul <md.sazzadul.islam15@gmail.com>
2022-10-26 16:54:10 +0200

    Updates

---
+++ precache-manifest.e50b201c97927004c8e64715a0c68078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5204c8620cf83dd25c40",
+    "url": "/ema-john-simple/static/css/main.07512114.chunk.css"
+  },
+  {
+    "revision": "5204c8620cf83dd25c40",
+    "url": "/ema-john-simple/static/js/main.5204c862.chunk.js"
+  },
+  {
+    "revision": "836e4380fe3f7816a983",
+    "url": "/ema-john-simple/static/js/1.836e4380.chunk.js"
+  },
+  {
+    "revision": "bec59a59489e5bb40a2c",
+    "url": "/ema-john-simple/static/js/2.bec59a59.chunk.js"
+  },
+  {
+    "revision": "d1190fbc63ac1b2e156a",
+    "url": "/ema-john-simple/static/js/runtime~main.d1190fbc.js"
+  },
+  {
+    "revision": "af182446517842baab598545221166e6",
+    "url": "/ema-john-simple/static/media/Logo.af182446.svg"
+  },
+  {
+    "revision": "61868bcb3bae278e0d8725bb3949296a",
+    "url": "/ema-john-simple/index.html"
+  }
+];
